5d0954948d2fd72026b6542d7d9f3232d88f3321
M4RT1N <mjlynch35@yahoo.com>
2023-05-03 11:15:10 -0500

    Added plant route to api/index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 // const plantRoutes = require("./plantRoutes");
 const userRoutes = require("./userRoutes");
+const plantRoutes = require("./plantRoutes");
 
-// router.use("/plants", plantRoutes);
+router.use("/plants", plantRoutes);
 router.use("/users", userRoutes);
 
 module.exports = router;
