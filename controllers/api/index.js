70e82531f2e6e49cce1ee74e0c4c8e514feff2ed
M4RT1N <mjlynch35@yahoo.com>
2023-05-03 19:21:02 -0500

    bugs

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require("./userRoutes");
+const plantRoutes = require("./plantRoutes");
 
 router.use("/users", userRoutes);
+
+router.use("/plants", plantRoutes);
 
 module.exports = router;
