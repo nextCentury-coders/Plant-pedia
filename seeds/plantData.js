c825c06a0f16aa282cace545170e2476102a4faf
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-05-07 19:15:51 -0500

    seeded plants data

--- seeds/plantData.js
+++ seeds/plantData.js
@@ -116,6 +116,70 @@ const plantData = [
         indoor_outdoor: 'Indoor',
         zone: 'N/A (tropical plant)'
         },
+        {
+          plant_image: 'https://images.unsplash.com/photo-1594417447205-5e1a4e8f05de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+          plant_name: 'Daisy',
+          difficulty: 1,
+          watering: 'Regularly, once or twice a week',
+          sun: 'Full sun or partial shade',
+          indoor_outdoor: 'Outdoor',
+          zone: '4-9'
+          },
+        {
+          plant_image: 'https://images.unsplash.com/photo-1629984957815-d5476f9a9146?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+          plant_name: 'Begonia',
+          difficulty: 2,
+          watering: 'Regularly, once or twice a week',
+          sun: 'partial shade',
+          indoor_outdoor: 'Indoor/Outdoor',
+          zone: '9-11'
+          },
+          {
+          plant_image: 'https://images.unsplash.com/photo-1590377663350-cfc38d4b6e43?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
+          plant_name: 'Iris',
+          difficulty: 2,
+          watering: 'Regularly, once or twice a week',
+          sun: 'Full sun or partial shade',
+          indoor_outdoor: 'Outdoor',
+          zone: '2-11'
+          },
+          {
+          plant_image: 'https://images.unsplash.com/photo-1616075196566-f2fc53eb6f75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80',
+          plant_name: 'Geranium',
+          difficulty: 2,
+          watering: 'Regularly, once or twice a week',
+          sun: 'Full sun or partial shade',
+          indoor_outdoor: 'Outdoor/Indoor',
+          zone: '10-11'
+          },
+            {
+            plant_image: 'https://images.unsplash.com/photo-1541093113199-a2e9d84e903f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXx6cXd0ejZncFJFQXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
+            plant_name: 'Chrysanthemum',
+            difficulty: 2,
+            watering: 'Regularly, once or twice a week',
+            sun: 'Full sun or partial shade',
+            indoor_outdoor: 'Outdoor/Indoor',
+            zone: '5-9'
+            },
+            {
+              plant_image: 'https://images.unsplash.com/photo-1637538286398-10173661522b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+              plant_name: 'Poinsettia',
+              difficulty: 2,
+              watering: 'Sparingly, every 1-2 weeks',
+              sun: 'Indirect sunlight',
+              indoor_outdoor: 'Indoor',
+              zone: '9-11'
+              },
+              {
+                plant_image: 'https://images.unsplash.com/photo-1650809373203-22461d5d82dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
+                plant_name: 'Azalea',
+                difficulty: 2,
+                watering: 'Regularly, once or twice a week',
+                sun: 'Partial shade',
+                indoor_outdoor: 'Outdoor/Indoor',
+                zone: '6-9'
+                },
+  
 
 ]
 const seedPlants = async () => {
